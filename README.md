677ab30b86831cb2b37de9172242b5d8a8973f9d
kiko <fcpenet@gmail.com>
2019-07-25 19:35:52 +0800

    added read me and the recognizer code

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # ai2project
+
+## For macOs
+
+### Pre-requirements
+1. Python 3.6.1
+2. virtualenv package. To install: pip install virtualenv
+
+### Install
+1. Make sure to create a virtual environment:
+python -m virtualenv <name of virtualenv>
+ex. python -m virtualenv ai2  # ai2 is the name of the virtual env.
+
+2. Enter virtual environment:
+source <name of virtual env>/bin/activate
+ex. source ai2/bin/activate
+
+3. Make sure you are in the root directory
+
+4. Install all dependencies:
+pip install -r requirements.txt
+
+5. try to run:
+python recognizer.py
+
+
+## For windows
+
+### Pre-requirements
+1. at least Python 3.6.1
+2. virtualenvwrapper-win package. To install: pip install virtualenvwrapper-win
+
+### Install
+1. Make sure to create a virtual environment:
+mkvirtualenv <name of virtualenv>
+ex. mkvirtualenv ai2  # ai2 is the name of the virtual env.
+
+2. Enter virtual environment:
+workon <name of virtual env>
+ex. workon ai2
+
+3. Make sure you are in the root directory
+
+4. Install all dependencies:
+pip install -r requirements.txt
+
+5. try to run:
+python recognizer.py
---
+++ program/recognizer.py
@@ -0,0 +1,103 @@
+import random
+import time
+
+import speech_recognition as sr
+import pyttsx3 as pyt
+
+
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
+
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        print("Listening")
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+def say(engine, text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+if __name__ == "__main__":
+    #parameters needed to collect
+    PARAMS = ["first name", "last name", "street number", "street name", "city"]
+    PARAMS = ["first name", "last name"]
+    values = {}
+    PROMPT_LIMIT = 5
+
+
+    # create recognizer and mic instances
+    engine = pyt.init()
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+
+    for p in PARAMS:
+        # ask for params via text to speech
+        while True:
+            for j in range(PROMPT_LIMIT):
+                say(engine, f'Please state your {p}')
+                value = recognize_speech_from_mic(recognizer, microphone)
+                if value["transcription"]:
+                    break
+                if not value["success"]:
+                    break
+                say(engine, "I didn't catch that. What did you say?\n")
+
+            # if there was an error, stop the program
+            if value["error"]:
+                print("ERROR: {}".format(value["error"]))
+                break
+
+            
+            say(engine, f"You said: {value['transcription']}. Is this correct?")
+            ans = recognize_speech_from_mic(recognizer, microphone)
+            print(ans)
+            if ans['transcription'] == "yes":
+                values[p] = value['transcription']
+                break
+            else:
+                say(engine, "Let's try again for {p}.")
+
+    print(values)
+    say(engine, f"You are {values['first name']} {values['last name']}")
